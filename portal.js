1a2d4be0723206d7b95edc8652987a327bbc84da
SINISTER-code <72219493+SINISTER-code@users.noreply.github.com>
2021-05-24 15:09:00 +0530

    Update portal.js
    spelling error

--- portal.js
+++ portal.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Save Corals", "Help Marine lives", "Save Oceans", "Help Enviroment", "Help you donate"],
+        strings: ["Save Corals", "Help Marine lives", "Save Oceans", "Help Environment", "Help you donate"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Save Corals", "Help Marine lives", "Save Oceans", "Help Enviroment", "Help you donate"],
+        strings: ["Save Corals", "Help Marine lives", "Save Oceans", "Help Environment", "Help you donate"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
